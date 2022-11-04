d537fc91ad01cb0a29bf5bb08c7c01f027ff026a
DESKTOP-LG384Q6\razib <razibdumham@live.com>
2022-11-04 11:37:19 +1100

    Domain model added

---
+++ PremiumCalculation.Domain/Rating.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PremiumCalculation.Domain
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public string RatingTitle { get; set; }
+        public decimal Factor { get; set; }
+
+        public ICollection<Occupation> Occupations { get; set; }
+    }
+}
