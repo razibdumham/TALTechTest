b71a4aaa2343a679fcb41e5a1ac12c82d8b6b63d
DESKTOP-LG384Q6\razib <razibdumham@live.com>
2022-11-04 12:01:03 +1100

    Add connection string, update db context and add migration

---
+++ PremiumCalculation.Infrastructure/Migrations/20221104005855_Occupation-seed-data.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PremiumCalculation.Infrastructure.Migrations
+{
+    public partial class Occupationseeddata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rating",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RatingTitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Factor = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rating", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Occupations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OccupationTitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RatingId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Occupations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Occupations_Rating_RatingId",
+                        column: x => x.RatingId,
+                        principalTable: "Rating",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Rating",
+                columns: new[] { "Id", "Factor", "RatingTitle" },
+                values: new object[,]
+                {
+                    { 1, 1m, "Professional" },
+                    { 2, 1.25m, "White Collar" },
+                    { 3, 1.50m, "Light Manual" },
+                    { 4, 1.75m, "Heavy Manual" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Occupations",
+                columns: new[] { "Id", "OccupationTitle", "RatingId" },
+                values: new object[,]
+                {
+                    { 2, "Doctor", 1 },
+                    { 3, "Author", 2 },
+                    { 1, "Cleaner", 3 },
+                    { 6, "Florist", 3 },
+                    { 4, "Farmer", 4 },
+                    { 5, "Mechanic", 4 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Occupations_RatingId",
+                table: "Occupations",
+                column: "RatingId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Occupations");
+
+            migrationBuilder.DropTable(
+                name: "Rating");
+        }
+    }
+}
--- PremiumCalculation.Infrastructure/PremiumCalculationDBContext.cs
+++ PremiumCalculation.Infrastructure/PremiumCalculationDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PremiumCalculation.Domain;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,6 +12,8 @@ namespace PremiumCalculation.Infrastructure
         {
 
         }
+
+        public virtual DbSet<Occupation> Occupations { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
