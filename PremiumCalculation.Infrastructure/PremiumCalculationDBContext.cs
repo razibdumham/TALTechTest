cf32fb7c8dd33707a8d8089e56bb2f85bc200bca
DESKTOP-LG384Q6\razib <razibdumham@live.com>
2022-11-04 11:30:39 +1100

    Update db context and startup class

--- PremiumCalculation.Infrastructure/PremiumCalculationDBContext.cs
+++ PremiumCalculation.Infrastructure/PremiumCalculationDBContext.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PremiumCalculation.Infrastructure
 {
-    class PremiumCalculationDBContext
+    public class PremiumCalculationDBContext:DbContext
     {
+        public PremiumCalculationDBContext(DbContextOptions<PremiumCalculationDBContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
