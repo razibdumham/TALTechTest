69389af276302b8dcecf29fa085c5110014b38e3
DESKTOP-LG384Q6\razib <razibdumham@live.com>
2022-11-05 14:12:57 +1100

    Add rating model decimal property attribute for fixing EF command error

--- PremiumCalculation.Domain/Rating.cs
+++ PremiumCalculation.Domain/Rating.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace PremiumCalculation.Domain
@@ -8,6 +9,7 @@ namespace PremiumCalculation.Domain
     {
         public int Id { get; set; }
         public string RatingTitle { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
         public decimal Factor { get; set; }
 
         public ICollection<Occupation> Occupations { get; set; }
